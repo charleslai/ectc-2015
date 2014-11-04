05389c29bf40b498ca1f6378411164467fc01751
knowlonix <dtknowlton@gmail.com>
2014-11-04 13:03:58 -0500

    Cleaning up professionals page.

--- dev/wordpress/wp-content/themes/ectc_theme/page-professionals.php
+++ dev/wordpress/wp-content/themes/ectc_theme/page-professionals.php
@@ -8,29 +8,17 @@
     </div>
 </div>
 <div class="row">
-        <?php // The Query
-          $the_query = new WP_query(array ( 'category_name' => 'Professional', 'posts_per_page' => -1) );
-          // The Loop
-          while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-            <div class="large-4 columns panel">
+<?php
+  $the_query = new WP_query(array ( 'category_name' => 'Professional', 'posts_per_page' => -1) );
+  // The Loop
+  while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+    <div class="large-4 columns panel">
+      <h3><?php the_title(); ?></h3>
+      <?php the_excerpt(); ?>
+      <p><?php the_tags() ?></p>
 
-            <?php 
-              //echo "<tr WPid='".get_the_ID()."' ";
-              $tags = wp_get_post_tags(get_the_ID(), array( 'fields' => 'names' ));
-              if(array_search('completed', $tags) !== FALSE) {
-                //echo "class='selected'><td class='toggle'><i class='icon-check'></i></td>";
-              } else {
-                //echo "><td class='toggle'><i class='icon-check-empty'></i></td>";
-              }
-              ?>
-              <h3><?php the_title(); ?></h3>
-              <?php the_excerpt(); ?>
-              <p><?php the_tags() ?></p>
-
-              </div>
-
-        <?php endwhile; ?>
-
+    </div>
+  <?php endwhile; ?>
 </div>
 
 <?php include("footer.php"); ?>
